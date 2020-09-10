2553e03414df7361cb134c4ece899d5a6b5dd93e
Sarthak Jain <sarthakjain7@outlook.com>
2020-09-10 09:53:44 +0530

    Added deletion at index function in deletion function and added a totalNodes function

--- LinkedList.c
+++ LinkedList.c
@@ -7,6 +7,19 @@ struct linkedl
     struct linkedl *next;
 };
 struct linkedl *head = NULL;
+int index=0;
+
+void totalNodes(int index)
+{
+    struct linkedl *ptr=head;
+    while (ptr!=NULL)
+    {
+        ptr=ptr->next;
+        index++;
+    }
+    printf("No. of nodes in linked list is/are %d\n",index);
+    
+}
 
 void displayLL()
 {
@@ -50,14 +63,19 @@ void insertionLL(int ele)
     }
     if (choice == 3)
     {
-        int i=1;
+        int i=0;
         printf("Enter the index after which node to be inserted\n");
         scanf("%d",&inbw_choice);
-        while (i!=inbw_choice)
+            while (i!=inbw_choice)
+        {
             ptr=ptr->next;
+            i++;
+        }    
         nodeptr->next=ptr->next;
         ptr->next=nodeptr;
         printf("%d successfully added after the index %d\n", ele,inbw_choice);
+        
+        
 
     }
     if (choice == 4) 
@@ -74,7 +92,7 @@ void insertionLL(int ele)
 
 void deletionLL()
 {
-    int choice=0,deleted_data=0,index=1,i=1;
+    int choice=0,deleted_data=0,deletion_index=0,i=1;
     struct linkedl *ptr1 = head;
     struct linkedl *ptr2 = head->next;
     printf("From Where do u want to delete the element\n1.From the beginning\n2.From the end\n3.From the given index in between the Linkedlist\n4.First occurence of given value\n");    
@@ -98,13 +116,41 @@ void deletionLL()
         free(ptr2);
         printf("%d successfully deleted from the end\n",deleted_data);
     }
-   
+     if (choice == 3)
+    {
+        printf("Enter the index of element to be deleted\n");
+        scanf("%d",&deletion_index);
+        if(deletion_index==0)
+        {
+            deleted_data=head->data;
+            head=head->next;
+            free(ptr1);
+            printf("%d successfully deleted from the index %d\n",deleted_data,deletion_index);
+        }
+        else if(deletion_index==1)
+        {
+            deleted_data=ptr2->data;
+            ptr1->next=ptr2->next;
+            free(ptr2);
+            printf("%d successfully deleted from the index %d\n",deleted_data,deletion_index);    
+        }
+        else
+        {
+        while (i<deletion_index)
+        {
+            ptr2=ptr2->next;
+            ptr1=ptr1->next;
+            i++;    
+        }
+        deleted_data=ptr2->data;
+        ptr1->next=ptr2->next;
+        free(ptr2);
+        printf("%d successfully deleted from the index %d\n",deleted_data,deletion_index);
+        }
 
-
+    }
 }
-
-
-
+ 
 void main()
 {
     int choice, insert_ele;
@@ -117,14 +163,17 @@ void main()
         {
         case 1:
             displayLL();
+            totalNodes(index);
             break;
         case 2:
             printf("Enter the data to be inserted\n");
             scanf("%d", &insert_ele);
             insertionLL(insert_ele);
+            totalNodes(index);
             break;
         case 3:
             deletionLL();
+            totalNodes(index);
             break;    
         case 6:
             printf("Successfully Exited\n");
