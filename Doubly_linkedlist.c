b62fb57e5e43641eb2891836a6047b808f46cd36
Sarthak Jain <sarthakjain7@outlook.com>
2020-10-01 10:38:08 +0530

    Added inserrion in the beginning in insertion function

--- Doubly_linkedlist.c
+++ Doubly_linkedlist.c
@@ -25,10 +25,19 @@ void insertionDLL(int num)
                 node->previous=NULL;
                 head=node;
         }
-        // else
-        // {
-        //         printf("Where do you want to store the Element\n");
-        // }
+        else
+        {
+                printf("Where do you want to store the Element\n1. In the Beginning\n2. At the End\n 3. After The given index\n4. Before the given index\n5. After a given value\n6. Before a given value\n");
+                scanf("%d",&choice);
+
+                if(choice==1)
+                {
+                        node->next=head;
+                        node->previous=NULL;
+                        head->previous=node;
+                        head=node;
+                }
+        }
         
 
 }
