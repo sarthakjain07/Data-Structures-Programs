ed42e92a2071ed395407663bd724d799d9cf44d2
Sarthak Jain <sarthakjain7@outlook.com>
2020-12-15 11:53:00 +0530

    Started for stack

---
+++ Stack/stack.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct stack{
+    int size;
+    int top;
+    int *a;
+}
+
+struct stack s;
+s.size=5;
+s.top=-1;
+s.a=(int *)malloc(size.s*sizeof(int));
+
+
+
+
