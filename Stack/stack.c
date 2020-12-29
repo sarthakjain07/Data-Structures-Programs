b197ea33e0842645825e2aae1ea0d9130c91fc8e
Sarthak Jain <sarthakjain7@outlook.com>
2020-12-29 15:27:26 +0530

    Modified Stack

--- Stack/stack.c
+++ Stack/stack.c
@@ -5,12 +5,12 @@ struct stack{
     int size;
     int top;
     int *a;
-}
+};
 
-struct stack s;
-s.size=5;
-s.top=-1;
-s.a=(int *)malloc(size.s*sizeof(int));
+struct stack *s;
+s->size=5;
+s->top=-1;
+s->a=(int *)malloc(size.s*sizeof(int));
 
 
 
