61ba55b6d2f6ec9f7f7c681673867cce60574066
Sarthak Jain <sarthakjain7@outlook.com>
2021-01-02 19:41:15 +0530

    Edited menu function

--- Stack/stack_using_linkedlist.c
+++ Stack/stack_using_linkedlist.c
@@ -13,19 +13,20 @@ struct LL *top=NULL;
 
 int main()
 {
+    int choice,ele;
      do
     {
     printf("Welcome to the program of stack\nEnter your choice\n1. Traverse\n2. Push\n3. Pop\n4. peek\n5. Exit\n");
     scanf("%d",&choice);
         switch (choice)
         {
-        case 1: traverse(s);
+        case 1: traverse();
                 break;
-        case 2: push(s,ele);
+        case 2: push(ele);
                 break;
-        case 3:pop(s);
+        case 3:pop();
             break;
-         case 4:peek(s);
+         case 4:peek();
              break;
         case 5: printf("Successfully Exited\n");
             break;               
