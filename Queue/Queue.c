6348093e56329430d731f5c33be33f448d80499b
Sarthak Jain <sarthakjain7@outlook.com>
2021-01-10 19:35:46 +0530

    Added enqueue function to add elements to queue and traverse function to display the elements inside the queue

--- Queue/Queue.c
+++ Queue/Queue.c
@@ -22,6 +22,29 @@ int isEmpty(struct queue *q)
     return 0;    
 }
 
+void enqueue(struct queue *q,int ele)
+{
+    if(isFull(q))
+        printf("Queue overflow\n");
+    else
+    {
+        q->rear++;
+        q->a[q->rear]=ele;
+    }
+}
+
+void traverse(struct queue *q)
+{
+    if(isEmpty(q))
+        printf("Queue underflow\n");
+    else
+    {
+        printf("Elements of queue are:\n");
+        for(int i=0; i<=q->rear;i++)
+            printf("%d\n",q->a[i]);
+    }
+}
+
 int main()
 {
     int choice,ele;
