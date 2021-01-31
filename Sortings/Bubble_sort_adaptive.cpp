0bdf9d56fd91e831e98509e94f1fd8b1ed7273f6
Sarthak Jain <sarthakjain7@outlook.com>
2021-01-31 14:50:30 +0530

    Modified bubble sort

--- Sortings/Bubble_sort_adaptive.cpp
+++ Sortings/Bubble_sort_adaptive.cpp
@@ -4,24 +4,26 @@ using namespace std;
 
 int main()
 {
-    int n,x=0,i;
+    int n,x=0,i,isSorted=0;
     cin>>n;
     int a[n];
     for (i = 0; i < n; i++)
         cin>>a[i];
-    while(n-x!=1)
+    while(n-x!=1) // decreasing with every x iteration
     {
-        i=0;
-        for (int j = i+1; j < n-x; j++)
+        isSorted=1;
+        for (i=0; i < n-x; i++) // decreasing with every (n-1) iteration
         {
-            if(a[i]>a[j])
+            if(a[i]>a[i+1])
             {
                 int temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;
+                a[i]=a[i+1];
+                a[i+1]=temp;
+                isSorted=0;
             }
-            i++;
         }
+        if(isSorted)
+            break;
         x++;    
     }
     for (int  i = 0; i < n; i++)
