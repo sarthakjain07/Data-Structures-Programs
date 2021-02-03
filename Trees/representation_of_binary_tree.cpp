79f2b4ab1862ce660e3f5004d845778e21b3c120
Sarthak Jain <sarthakjain7@outlook.com>
2021-02-03 15:57:29 +0530

    added comments

--- Trees/representation_of_binary_tree.cpp
+++ Trees/representation_of_binary_tree.cpp
@@ -1,20 +1,19 @@
 #include<iostream>
 using namespace std;
 
-
-
+// structure of binary trees
 struct bt{
     int data;
     struct bt *left;
     struct bt *right;
 };
 
-
-
+// function to create all the nodes of binary trees
 struct bt *createNode(int data)
 {
     struct bt *node=(struct bt*)malloc(sizeof(struct bt));
-    node->data=data;
+    node->data=data;//assigning data to the node
+    // assigning the left and right pointers value NULL initially
     node->left=NULL;
     node->right=NULL;
     return node;
@@ -22,12 +21,12 @@ struct bt *createNode(int data)
 
 int main()
 {
-    
+    // creating root node along with the nodes n1 and n2
     struct bt *root=createNode(1);
     struct bt *n2=createNode(3);
     struct bt *n1=createNode(2);
 
-    
+    // linking the nodes n1 and n2 with the root nodes   
     root->left=n1;
     root->right=n2;
     return 0;
