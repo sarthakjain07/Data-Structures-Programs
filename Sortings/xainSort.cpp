83d56060c6a10a2de62cbdb54223e673d682a33b
Sarthak Jain <sarthakjain7@outlook.com>
2021-03-08 10:29:11 +0530

    Modified xain sort with the time complexity of O(nlogn)

--- Sortings/xainSort.cpp
+++ Sortings/xainSort.cpp
@@ -1,34 +1,76 @@
 #include<iostream>
 using namespace std;
 
-int minPosition(int *a, int size)
-{
-    int min=INT_MAX,min_po;
-    for (int i = 0; i < size; i++)
-    {
-        if(a[i]<min)
-        {
-            min=a[i];
-            min_po=i;
-        }
-    }
-    return min_po;
-}
+// int minPosition(int *a, int size)
+// {
+//     int min=INT_MAX,min_po;
+//     for (int i = 0; i < size; i++)
+//     {
+//         if(a[i]<min)
+//         {
+//             min=a[i];
+//             min_po=i;
+//         }
+//     }
+//     return min_po;
+// }
+
+// void xainSort(int *a, int size)
+// {
+//     int min_po,i;
+//     int *b=(int *)malloc(size*sizeof(int));
+//     for (i = 0; i < size; i++)
+//     {
+//         min_po=minPosition(a,size);
+//         b[i]=a[min_po];
+//         a[min_po]=INT_MAX;
+//     }
+//     for (i = 0; i < size; i++)
+//         a[i]=b[i];
+    
+
+// }
 
 void xainSort(int *a, int size)
 {
-    int min_po,i;
+    int min,max,i,j,pos;
     int *b=(int *)malloc(size*sizeof(int));
-    for (i = 0; i < size; i++)
+    if(size%2==0)
     {
-        min_po=minPosition(a,size);
-        b[i]=a[min_po];
-        a[min_po]=INT_MAX;
+        //for min elements
+        for(i=0;i<size/2;i++)
+        {
+            min=INT_MAX;
+            for(j=0;j<size;j++)
+            {
+                if(a[j]<min)
+                {
+                    min=a[j];
+                    pos=j;
+                }
+            }
+            b[i]=min;
+            a[pos]=INT_MAX;
+        }
+        // for max elements
+        for(i=size-1;i>=size/2;i--)
+        {
+            max=INT_MIN;
+            for(j=0;j<size;j++)
+            {
+                if(a[j]>max && a[j]!=INT_MAX)
+                {
+                    max=a[j];
+                    pos=j;
+                }
+            }
+            b[i]=max;
+            a[pos]=INT_MIN;
+        }
     }
+    // shifting all elements to originl array after sorting
     for (i = 0; i < size; i++)
         a[i]=b[i];
-    
-
 }
 
 int main()
@@ -40,8 +82,6 @@ int main()
         cin >> a[i];
     // logic for xain special technique
     xainSort(a,size);
-
-
     for (i = 0; i < size; i++)
         cout << a[i] << " ";
     return 0;
